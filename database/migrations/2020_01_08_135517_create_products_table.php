3c8ac96c4002f5629ff01c5f1604a1b6fac80dac
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-08 16:24:26 +0200

    created products table

---
+++ database/migrations/2020_01_08_135517_create_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('sub_category_id');
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->bigInteger('price');
+            $table->float('width')->nullable();
+            $table->float('height')->nullable();
+            $table->timestamps();
+
+            $table->index(['sub_category_id', 'price']);
+            $table->foreign('sub_category_id')->references('id')->on('sub_categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign(['sub_category_id']);
+        });
+        Schema::dropIfExists('products');
+    }
+}
