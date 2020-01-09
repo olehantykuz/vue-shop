58103abab2ec1ae47964ca0571e8c73452ae5734
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-09 10:33:15 +0200

    created admin blade templates

---
+++ resources/views/admin/products/list.blade.php
@@ -0,0 +1,33 @@
+@extends('admin.index')
+
+@section('title', 'List of Products')
+
+<style>
+    table {
+        width: 50%;
+    }
+    td {
+        text-align: center;
+        border: 1px solid grey;
+    }
+</style>
+
+@section('content')
+    <h3>List of Products</h3>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Sub Category</th>
+            <th>Product name</th>
+            <th>Price, $</th>
+        </tr>
+        @foreach($products as $product)
+            <tr>
+                <td>{{ $product->id }}</td>
+                <td>{{ $product->subCategory->name }}</td>
+                <td>{{ $product->name }}</td>
+                <td>{{ $product->price / 100 }}</td>
+            </tr>
+        @endforeach
+    </table>
+@endsection
