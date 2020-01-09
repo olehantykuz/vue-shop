f83807eaa8f136d5ce7dc249cdd60585a83ab5bf
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-09 10:34:39 +0200

    added admins routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::group(['prefix' => 'products'], function () {
+        Route::get('/', 'ProductController@index');
+    });
+});
