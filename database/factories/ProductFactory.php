cc5d1fd095d43590c955db2bcdcef131c986dbc7
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-09 09:30:22 +0200

    fixed name in product factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -5,13 +5,14 @@
 use App\Models\Product;
 use App\Models\SubCategory;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(Product::class, function (Faker $faker) {
     $ids = SubCategory::all()
         ->pluck('id');
 
     return [
-        'name' => $faker->name,
+        'name' => Str::random(10),
         'price' => rand(1, 999999),
         'sub_category_id' => (int) $ids->random(),
         'description' => $faker->text(500),
