7e0b4daf801f9af3b9208faf59ac29675996422e
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-10 14:06:08 +0200

    added switch currency to products list page

--- resources/views/admin/products/list.blade.php
+++ resources/views/admin/products/list.blade.php
@@ -1,6 +1,10 @@
 @extends('admin.index')
 
 @section('title', 'List of Products')
+@php
+$currencyName = session()->get('currency.name', 'USD');
+$currencyRate = session()->get('currency.rate', 1);
+@endphp
 
 <style>
     table {
@@ -13,20 +17,28 @@
 </style>
 
 @section('content')
+    <ul id = "currency_menu">
+        @foreach($currencies as $currency)
+            <li class="currency {{ $currency->name == $currencyName ? 'active' : '' }}">
+                <a href ="{{ url('/admin/currencies/' . $currency->name) }}">{{ $currency->name }}</a>
+            </li>
+        @endforeach
+    </ul>
+
     <h3>List of Products</h3>
     <table>
         <tr>
             <th>ID</th>
             <th>Category</th>
             <th>Product name</th>
-            <th>Price, $</th>
+            <th>Price, {{ $currencyName }}</th>
         </tr>
         @foreach($products as $product)
             <tr>
                 <td>{{ $product->id }}</td>
                 <td>{{ $product->category->name }}</td>
                 <td>{{ $product->name }}</td>
-                <td>{{ $product->price / 100 }}</td>
+                <td>{{ round($product->price * $currencyRate / 100, 2) }}</td>
             </tr>
         @endforeach
     </table>
