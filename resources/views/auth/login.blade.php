5e7096e2bbea2a6d9816794239f6b3672d00e047
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-13 11:09:23 +0200

    create login form

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.index')
+
+@section('content')
+    <form method="POST" action="/login">
+    @csrf
+        <div class="form-group">
+            <label for="email">
+                Email address <span class="required">*</span>
+            </label>
+            <input id="email" type="email" name="email" class="@error('email') is-invalid @enderror">
+        </div>
+        <div class="form-group">
+            <label for="password">
+                Password <span class="required">*</span>
+            </label>
+            <input id="password" type="password" name="password" class="@error('password') is-invalid @enderror">
+        </div>
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <input type="submit" value="Login">
+    </form>
+@endsection
