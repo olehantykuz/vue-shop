29469e9c0d9293f9d283ad07e9de8fdbdada0d7e
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-13 10:14:26 +0200

    updated index layout

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -3,24 +3,28 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <title>Admin - @yield('title')</title>
 </head>
 <body>
-<div>
-    <ul>
-        @guest
-            <li><a href="#">Register</a></li>
-            <li><a href="#">Login</a></li>
-        @endguest
-        <li><a href="{{ route('products.list') }}">Products</a></li>
-        <li><a href="{{ route('categories.list') }}">Categories</a></li>
-    </ul>
-</div>
-<div class="content">
-    <div class="container">
-        @yield('content')
+    <div>
+        <ul>
+            @guest
+                <li><a href="#">Register</a></li>
+                <li><a href="#">Login</a></li>
+            @endguest
+            <li><a href="{{ route('products.list') }}">Products</a></li>
+            <li><a href="{{ route('categories.list') }}">Categories</a></li>
+        </ul>
     </div>
-</div>
+    <div class="content">
+        <div class="container">
+            @yield('content')
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>
