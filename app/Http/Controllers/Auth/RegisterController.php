c8a8fba3d113e0f39a74d775b520524a89ffdd00
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-13 11:17:23 +0200

    updated redirectTo property for auth controllers

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -31,7 +31,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/admin/products';
+    protected $redirectTo = RouteServiceProvider::PRODUCTS;
 
     /**
      * Create a new controller instance.
