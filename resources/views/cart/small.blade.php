391da3a792633119d2f0880b299135edc8ddf28d
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-13 15:44:26 +0200

    added link for shopping cart

--- resources/views/cart/small.blade.php
+++ resources/views/cart/small.blade.php
@@ -1,3 +1,3 @@
 @if(session()->get('cart'))
-    <div>Cart has {{ array_sum(session()->get('cart')) }} items. <a href="#">Detail</a></div>
+    <div>Cart has {{ array_sum(session()->get('cart')) }} items. <a href="{{ route('cart.detail') }}">Detail</a></div>
 @endif
