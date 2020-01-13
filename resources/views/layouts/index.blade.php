434814fa30f4cc34137abe80945ddd183bac9cf9
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-13 11:19:29 +0200

    updated index template

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -9,10 +9,12 @@
 </head>
 <body>
     <div>
+        @auth<div>Hi, {{ Auth::user()->name }}</div>@endauth
         <ul>
             @guest
+                <li><a href="/">Home</a></li>
                 <li><a href="/register">Register</a></li>
-                <li><a href="#">Login</a></li>
+                <li><a href="/login">Login</a></li>
             @endguest
             @auth
                     <li><a href="/logout">Logout</a></li>
