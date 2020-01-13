3932244c6df4124778e90e3922382194361aaa7d
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-13 10:21:04 +0200

    updated marcup for register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,26 +9,30 @@
 @section('content')
     <form method="POST" action="/register">
         @csrf
-
-        <label for="email">
-            Email address <span class="required">*</span>
-        </label>
-        <input id="email" type="email" name="email" class="@error('email') is-invalid @enderror">
-
-        <label for="name">
-            Your name <span class="required">*</span>
-        </label>
-        <input id="name" type="text" name="name" class="@error('name') is-invalid @enderror">
-
-        <label for="password">
-            Password <span class="required">*</span>
-        </label>
-        <input id="password" type="password" name="password" class="@error('password') is-invalid @enderror">
-
-        <label for="password_confirmation">
-            Confirm password <span class="required">*</span>
-        </label>
-        <input id="password_confirmation" type="password" name="password_confirmation" class="@error('password') is-invalid @enderror">
+        <div class="form-group">
+            <label for="email">
+                Email address <span class="required">*</span>
+            </label>
+            <input id="email" type="email" name="email" class="@error('email') is-invalid @enderror">
+        </div>
+        <div class="form-group">
+            <label for="name">
+                Your name <span class="required">*</span>
+            </label>
+            <input id="name" type="text" name="name" class="@error('name') is-invalid @enderror">
+        </div>
+        <div class="form-group">
+            <label for="password">
+                Password <span class="required">*</span>
+            </label>
+            <input id="password" type="password" name="password" class="@error('password') is-invalid @enderror">
+        </div>
+        <div class="form-group">
+            <label for="password_confirmation">
+                Confirm password <span class="required">*</span>
+            </label>
+            <input id="password_confirmation" type="password" name="password_confirmation" class="@error('password') is-invalid @enderror">
+        </div>
 
         @if ($errors->any())
             <div class="alert alert-danger">
