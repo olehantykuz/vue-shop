bc8ee5c35255eee28d22c0319c64c4dc86b1f4ab
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-14 12:25:53 +0200

    fixed hardcode of default value for currency name in currencyTrait

--- app/Http/Controllers/Traits/CurrencyTrait.php
+++ app/Http/Controllers/Traits/CurrencyTrait.php
@@ -7,7 +7,7 @@ trait CurrencyTrait
     protected function selectedCurrency()
     {
         return [
-            'currencyName' => session()->get('currency.name', 'USD'),
+            'currencyName' => session()->get('currency.name', config('app.default_currency')),
             'currencyRate' => session()->get('currency.rate', 1),
         ];
     }
