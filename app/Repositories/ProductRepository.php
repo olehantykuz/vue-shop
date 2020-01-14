5a490cd908a8db5d4a5a595352f56c5e83a2c02d
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-14 13:44:25 +0200

    created Product repository

---
+++ app/Repositories/ProductRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Repositories;
+use App\Models\Product;
+
+class ProductRepository
+{
+    /**
+     * @param int|null $count
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function all(?int $count = null)
+    {
+        return Product::with('category')
+            ->paginate($count);
+    }
+}
