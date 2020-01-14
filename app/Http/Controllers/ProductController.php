b15c1af8de1dcce7b95b44f8c8d853cc75eb6fe9
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-14 13:54:40 +0200

    used ProductService insteed model in ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,21 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Models\Currency;
-use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Traits\CurrencyTrait;
+use Illuminate\Support\Facades\Validator;
+use App\Services\ProductService;
 
 class ProductController extends Controller
 {
     use CurrencyTrait;
 
     /**
+     * @param ProductService $service
+     * @param Request $request
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function index()
+    public function index(ProductService $service, Request $request)
     {
-        $products = Product::with('category')
-            ->get();
+        /** @var \Illuminate\Contracts\Validation\Validator $validator */
+        $validator = Validator::make($request->all(), [
+            'count' => 'nullable|integer|min:1',
+        ]);
+        $validator->validate();
+
+        $products = $service->all($request->get('count'));
         $currencies = Currency::pluck('conversion_rate', 'name');
 
         return view('admin.products.list',
