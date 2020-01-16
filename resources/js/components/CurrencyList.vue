1f7ba2dbcf286a93e0abe3ffe3df81f261a010a8
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-16 17:07:54 +0200

    created CurrencyList component

---
+++ resources/js/components/CurrencyList.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="row">
+        <div class="col-2 offset-10">
+            <ul id="currency_menu" class="list-group">
+                <li
+                    is="currency-list-item"
+                    v-for="currency in currencies"
+                    :currency="currency"
+                >
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    import CurrencyListItem from "./CurrencyListItem";
+    export default {
+        name: "CurrencyList",
+        components: {
+            CurrencyListItem
+        },
+        data: function () {
+            return {
+                currencies: [
+                    {name: 'USD'},
+                    {name: 'EUR'},
+                    {name: 'UAH'},
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
