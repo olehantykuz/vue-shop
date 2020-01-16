e5ade4d8f4299b69fc75420cec199692f8987b8c
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-16 16:24:23 +0200

    created App component

---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <ExampleComponent />
+</template>
+
+<script>
+    import ExampleComponent from "./components/ExampleComponent";
+
+    export default {
+        name: "App",
+        components: {
+            ExampleComponent
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
