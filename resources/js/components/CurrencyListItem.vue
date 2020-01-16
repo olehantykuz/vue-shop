13ed2bcc4a16311b6f340ff1995ba9afe5cf127e
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-16 17:07:02 +0200

    created CurrencyListItem Component

---
+++ resources/js/components/CurrencyListItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <li class="list-group-item">
+        <a href ="#">{{ currency.name }}</a>
+    </li>
+</template>
+
+<script>
+    export default {
+        name: "CurrencyItem",
+        props: {
+            currency: {
+                type: Object,
+                required: true
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
