03910b9c7290a3f1308fae508897d0d0abe27ba4
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-17 14:13:07 +0200

    added eventBus and currencies to data of root element

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,8 +11,37 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 import App from "./App";
+import EventBus from "./event-bus";
+import { getCurrencies } from "./requests/currency";
 
 const app = new Vue({
     el: '#app',
+    data: {
+        currencies: {},
+        selectedCurrency: 'USD',
+    },
+    computed: {
+        currencyConversationRate: function () {
+            const key = this.selectedCurrency;
+            const currency = this.currencies[key];
+
+            return currency ? (currency.conversion_rate || 1) : 1;
+        }
+    },
+    methods: {
+        updateSelectedCurrency(payload) {
+            this.selectedCurrency = payload;
+        }
+    },
+    created() {
+        getCurrencies().then(response => {
+            this.currencies = response.data;
+        })
+    },
+    mounted () {
+        EventBus.$on('selectCurrency', payLoad => {
+            this.updateSelectedCurrency(payLoad);
+        });
+    },
     render: h => h(App)
 });
