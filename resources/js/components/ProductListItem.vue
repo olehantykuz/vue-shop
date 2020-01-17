d26c5d9787208238f7e0be5ff8b59539963bcccd
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-17 10:58:04 +0200

    created component ProductListItem

---
+++ resources/js/components/ProductListItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <tr>
+        <td>{{ product.id }}</td>
+        <td>{{ product.category.name }}</td>
+        <td>{{ product.name }}</td>
+        <td>{{ product.price }}</td>
+    </tr>
+</template>
+
+<script>
+    export default {
+        name: "ProductListItem",
+        props: {
+            product: {
+                type: Object,
+                required: true
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
