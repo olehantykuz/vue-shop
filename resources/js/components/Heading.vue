b8e4bd5e20fb99f043af94290fc1de5b81fd20b1
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-20 15:02:22 +0200

    created Heading component

---
+++ resources/js/components/Heading.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <div class="d-flex flex-row justify-content-between">
+            <ul class="list-group list-group-horizontal">
+                <li class="list-group-item">
+                    <router-link to="/" exact>Products</router-link>
+                </li>
+                <li class="list-group-item">
+                    <router-link to="/register">Register</router-link>
+                </li>
+                <li class="list-group-item">
+                    <router-link to="/login">Login</router-link>
+                </li>
+                <li class="list-group-item"><a href="#">Logout</a></li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "Heading"
+    }
+</script>
+
+<style scoped>
+
+</style>
