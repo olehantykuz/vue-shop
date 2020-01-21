23741ce00d47243afb01a42053a27fd4006f244c
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-21 13:59:05 +0200

    create UserService class

---
+++ app/Services/UserService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\UserRepository;
+use Illuminate\Support\Facades\Hash;
+
+class UserService
+{
+    /** @var UserRepository */
+    protected $repository;
+
+    public function __construct()
+    {
+        $this->repository = new UserRepository();
+    }
+
+    public function create(array $data)
+    {
+        return $this->repository->create([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password']),
+        ]);
+    }
+
+}
