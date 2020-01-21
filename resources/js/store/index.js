3bb243747b4129cc787de35a84ca9c4b9f190370
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-21 17:15:20 +0200

    create login module for store

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,9 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import login from "./modules/login";
+
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
+    modules: {
+        login
+    },
     state: {
         currencies: {},
         selectedCurrency: 'USD',
