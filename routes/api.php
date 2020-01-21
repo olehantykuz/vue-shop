93d9a562f933ef21c4239e8981f318a6c606c977
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-21 14:03:28 +0200

    added auth routes group

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['namespace' => 'App'], function () {
+    Route::group([
+        'middleware' => 'api',
+        'prefix' => 'auth'
+    ], function () {
+        Route::post('register', 'AuthController@register');
+        Route::post('login', 'AuthController@login');
+        Route::post('logout', 'AuthController@logout');
+        Route::post('refresh', 'AuthController@refresh');
+        Route::get('me', 'AuthController@me');
+    });
     Route::group(['prefix' => 'currencies'], function () {
         Route::get('/', 'CurrencyController@index');
     });
