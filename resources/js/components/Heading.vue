fad40728d305dd628b3aa69c6daec65eb603fe24
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-22 13:43:55 +0200

    added behavior for nav menu according to loggedIn status

--- resources/js/components/Heading.vue
+++ resources/js/components/Heading.vue
@@ -5,25 +5,29 @@
                 <li class="list-group-item">
                     <router-link :to="{name: 'home'}" exact>Products</router-link>
                 </li>
-                <li class="list-group-item">
+                <li class="list-group-item" v-if="!status.loggedIn">
                     <router-link :to="{name: 'register'}">Register</router-link>
                 </li>
-                <li class="list-group-item">
+                <li class="list-group-item" v-if="!status.loggedIn">
                     <router-link :to="{name: 'login'}">Login</router-link>
                 </li>
-                <li class="list-group-item">
+                <li class="list-group-item" v-if="status.loggedIn">
                     <a href="" @click.prevent="logout">Logout</a>
                 </li>
             </ul>
+            <div v-if="status.loggedIn">{{ account.email }}</div>
         </div>
     </header>
 </template>
 
 <script>
-    import { mapActions } from "vuex";
+    import { mapState, mapActions } from "vuex";
 
     export default {
         name: "Heading",
+        computed: {
+            ...mapState('user', ['status', 'account'])
+        },
         methods: {
             ...mapActions('user', ['logout'])
         }
