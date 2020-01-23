23d689539c642691e682eae4320dd751f423a40b
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-23 16:41:56 +0200

    created CartSmall component

---
+++ resources/js/components/CartSmall.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="d-flex flex-row list-group-item">
+        <div>Cart: {{totalCartItems}} items in cart</div>
+    </div>
+</template>
+
+<script>
+    import { mapGetters } from 'vuex';
+
+    export default {
+        name: "CartSmall",
+        computed: {
+            ...mapGetters('cart', ['totalCartItems']),
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
