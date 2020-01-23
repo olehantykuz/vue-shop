9cf124e71737522187a0722711999e8fcc4ebedc
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-23 16:45:24 +0200

    added button add to cart to product item

--- resources/js/components/ProductListItem.vue
+++ resources/js/components/ProductListItem.vue
@@ -4,12 +4,31 @@
         <td>{{ product.category.name }}</td>
         <td>{{ product.name }}</td>
         <td>{{ formattedPrice }}</td>
+        <td>
+            <input
+                class="count-input"
+                type="number"
+                min="1"
+                v-model="quantity"
+            />
+            <button
+                @click="addProduct"
+            >
+                Add to cart
+            </button>
+        </td>
     </tr>
 </template>
 
 <script>
+    import { mapActions } from 'vuex';
     export default {
         name: "ProductListItem",
+        data () {
+            return {
+                quantity: 1,
+            }
+        },
         props: {
             product: {
                 type: Object,
@@ -24,11 +43,24 @@
             formattedPrice: function () {
                 return parseFloat(this.product.price * this.rate / 100)
                     .toFixed(2);
+            },
+        },
+        methods: {
+            ...mapActions('cart', [
+                'addToCart'
+            ]),
+            addProduct() {
+                this.addToCart({
+                    product: Object.assign({}, this.product),
+                    quantity: this.quantity
+                })
             }
         }
     }
 </script>
 
 <style scoped>
-
+    .count-input {
+        max-width: 70px;
+    }
 </style>
