540df73cf6539d8fa8d52b430abf76f70c97b579
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-24 10:20:59 +0200

    implemented action removeFromCart

--- resources/js/store/modules/cart/index.js
+++ resources/js/store/modules/cart/index.js
@@ -3,60 +3,68 @@ const state = {
 };
 
 const getters = {
-    totalCartItems: state => state.items.length
+    totalCartItems: state => state.items.length,
+    cartProducts: state => state.items,
 };
 
 const actions = {
     addToCart({ commit, state }, { product, quantity }) {
         const id = product.id;
-        const cartItem = state.items.find(item => item.product && item.product.id === id);
+        const cartItem = findItemInCart(id);
+        const formattedQuantity = parseInt(quantity);
 
         if (!cartItem) {
-            commit('pushProductToCart', { product, quantity })
+            commit('pushItemToCart', { product, quantity: formattedQuantity })
         } else {
-            commit('incrementItemQuantity', {id: product.id, quantity})
+            commit('incrementItemQuantity', {id: product.id, quantity: formattedQuantity})
         }
     },
-    // removeFromCart({ commit, state }, { id, count }) {
-    //     const { cart } = state;
-    //     if (id in cart.items) {
-    //         if (count >= cart.items[id].count) {
-    //             delete  cart.items[id];
-    //         } else {
-    //             cart.items[id].count -= count;
-    //         }
-    //     }
-    //     commit('setCart', cart);
-    // },
+    removeFromCart({ commit, state }, { id, quantity }) {
+        const cartItem = findItemInCart(id);
+
+        if (cartItem) {
+            if (quantity >= cartItem.quantity) {
+                commit('removeItemFromCart', id)
+            } else {
+                commit('decrementItemQuantity', { id, quantity })
+            }
+        }
+    },
     clearCart({ commit }) {
         commit('clearCart')
     }
 };
 
 const mutations = {
-    pushProductToCart (state, { product, quantity }) {
+    pushItemToCart (state, { product, quantity }) {
         state.items.push({
             product,
             quantity
         })
     },
-
+    removeItemFromCart (state, id) {
+        const items = state.items.filter(item => {
+            return item.product.id != id;
+        });
+        state.items = items;
+    },
     incrementItemQuantity (state, { id, quantity }) {
-        const cartItem = state.items.find(item => item.product && item.product.id === id);
+        const cartItem = findItemInCart(id);
         cartItem.quantity += quantity;
     },
-
+    decrementItemQuantity (state, { id, quantity }) {
+        const cartItem = findItemInCart(id);
+        cartItem.quantity -= quantity;
+    },
     setCartItems (state, { items }) {
         state.items = items
-    },
-
-    setCart(state, payload) {
-        state.cart = payload;
     },
     clearCart(state) {
         state.items = [];
     }
 };
+
+const findItemInCart = id => state.items.find(item => item.product && item.product.id === id);
 
 export default {
     namespaced: true,
