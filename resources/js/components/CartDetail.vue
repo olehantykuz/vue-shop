9656a7734c781cafe0f3c3a9d0c7da1951849ebd
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-24 10:12:07 +0200

    created CartDetail component

---
+++ resources/js/components/CartDetail.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <h2>Shopping Cart</h2>
+        <table class="table">
+            <tr>
+                <th>#</th>
+                <th>Product Name</th>
+                <th>Price, {{ currency }}</th>
+                <th>Quantity</th>
+                <th>Total price, {{ currency }}</th>
+                <th>Remove</th>
+            </tr>
+            <tr
+                is="cart-detail-item"
+                v-for="(item, index) in cartProducts"
+                :key="item.product.id"
+                :product="item.product"
+                :quantity="item.quantity"
+                :rate="conversationRate"
+                :index="index"
+            />
+        </table>
+    </div>
+</template>
+
+<script>
+    import { mapGetters, mapState } from "vuex";
+    import CartDetailItem from "./CartDetailItem";
+
+    export default {
+        name: "CartDetail",
+        components: {
+            CartDetailItem
+        },
+        computed: {
+            ...mapGetters([
+                'conversationRate'
+            ]),
+            ...mapGetters('cart', ['cartProducts']),
+            ...mapState({
+                currency: state => state.selectedCurrency,
+            }),
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
